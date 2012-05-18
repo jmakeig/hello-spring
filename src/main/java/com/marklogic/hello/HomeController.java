ccfa175c1bfeef20e3456c2954b7998ac5f277a5
Justin Makeig <jmpublic@makeig.com>
2012-05-18 10:07:33 -0700

    Unused logger and import

--- src/main/java/com/marklogic/hello/HomeController.java
+++ src/main/java/com/marklogic/hello/HomeController.java
@@ -2,8 +2,6 @@ package com.marklogic.hello;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    //private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     private KnowledgeBase kb;
 
